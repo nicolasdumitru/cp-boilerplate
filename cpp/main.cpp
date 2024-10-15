0152896fad69686e4e5bc8d9e0485d3602352e6a
nicolasdumitru <nicolasdumitru13@gmail.com>
2024-10-15 22:24:05 +0300

    Update boilerplate data types, std(in|out|err) etc
    Remove cruft.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,34 +1,24 @@
 #include <cstddef>
 #include <cstdint>
-#include <fstream>
 #include <iostream>
-#include <istream>
-#include <ostream>
 
 using usize = std::size_t;
-using i8 = std::int8_t;  // cin treats this as char
-using u8 = std::uint8_t; // cin treats this as char
-using i16 = std::int16_t;
+using isize = std::ptrdiff_t;
+using u8 = std::uint8_t; // std input streams treat this as char
+using i8 = std::int8_t;  // std input streams treat this as char
 using u16 = std::uint16_t;
-using i32 = std::int32_t;
+using i16 = std::int16_t;
 using u32 = std::uint32_t;
-using i64 = std::int64_t;
+using i32 = std::int32_t;
 using u64 = std::uint64_t;
+using i64 = std::int64_t;
 
-using std::istream;
-using std::ostream;
+using std::cin;
+using std::cout;
+using std::cerr;
 
 int main() {
     std::ios::sync_with_stdio(false);
-
-    std::ifstream fin("file.in");
-    std::ofstream fout("file.out");
-
-    istream &in = std::cin;
-    ostream &out = std::cout;
-
-    fin.close();
-    fout.close();
 
     return 0;
 }
